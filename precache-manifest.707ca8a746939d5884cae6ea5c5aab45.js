f918f7d2129042ef80d7e9c2445e42f3a5728dfd
Thales Brederodes Montarroyos Candido <54904851+ThalesBMC@users.noreply.github.com>
2020-05-31 01:27:10 -0300

    Updates

---
+++ precache-manifest.707ca8a746939d5884cae6ea5c5aab45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841ab4fb832fd9285d9e7090c4be5cd6",
+    "url": "/Search-in-React/index.html"
+  },
+  {
+    "revision": "01a6e082de6cfb075d74",
+    "url": "/Search-in-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "683e0347219192ad28d2",
+    "url": "/Search-in-React/static/css/main.522f56fc.chunk.css"
+  },
+  {
+    "revision": "01a6e082de6cfb075d74",
+    "url": "/Search-in-React/static/js/2.7b0cb491.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Search-in-React/static/js/2.7b0cb491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683e0347219192ad28d2",
+    "url": "/Search-in-React/static/js/main.ff7b15d7.chunk.js"
+  },
+  {
+    "revision": "61227d1cdc5d9e087b2a",
+    "url": "/Search-in-React/static/js/runtime-main.b4b6653b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Search-in-React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
